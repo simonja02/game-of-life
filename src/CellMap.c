a0724278e6c122649db948d4245e8bc72642ed27
Josh Simon <simonja02@gmail.com>
2012-11-21 15:41:05 -0500

    Converted to pointer functions to remove global variables

--- src/CellMap.c
+++ src/CellMap.c
@@ -1,9 +1,9 @@
 #include "CellMap.h"
 
-void createMap() {
+void createMap(Cell map[][MAP_SIZE_Y]) {
     int i;
     int j;
-
+     
     for (i=0; i<MAP_SIZE_X; i++) {
         for (j=0; j<MAP_SIZE_Y; j++) {
             map[i][j] = newBlankCell(i, j);
@@ -11,7 +11,7 @@ void createMap() {
     }
 }
 
-void resetMap() {
+void resetMap(Cell map[][MAP_SIZE_Y]) {
     // Just create a new one, the map will be overwritten
-    createMap();
+    createMap(map);
 }
