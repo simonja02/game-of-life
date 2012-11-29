0d98bd61ed9771a088ad4abdb095e331cb7e6d5f
Josh Simon <simonja02@gmail.com>
2012-11-29 13:01:42 -0500

    Fixes #2. Modified import function to support # comments

--- src/MapImport.c
+++ src/MapImport.c
@@ -2,10 +2,13 @@
  */
 
 #include <stdio.h>
+#include <string.h>
 #include "CellMap.h"
 #include "Cell.h"
 #include "GameLogic.h"
 #include "Main.h"
+
+#define MAX_LINE_SIZE 100
 
 int importMapFromFile(Cell map[][MAP_SIZE_Y], char* filename) {
     FILE* input = fopen(filename, "r");
@@ -22,15 +25,25 @@ int importMapFromFile(Cell map[][MAP_SIZE_Y], char* filename) {
     // MAP_WIDTH MAP_HEIGHT
     // Xn Yn
 
-    int width, height; // ignored for now
-    fscanf(input, "%d %d\n", &width, &height);
-   
+    int width = -1, height = -1; // ignored for now
+    char buffer[MAX_LINE_SIZE];
+
     int x, y; 
-    while (fscanf(input, "%d %d\n", &x, &y) != EOF) {
-        // Ensure cell isn't already alive
-        Cell* c = getCellPointer(map, x, y);
-        if (!(*c).nextGenAlive) {
-            changeAliveStatus(map, ALIVE, x, y);
+    while (fgets(buffer, sizeof(buffer), input) != NULL) {
+        char* cp = strchr(buffer, '#');
+        if (cp) { *cp = '\0'; }
+
+        sscanf(buffer, "%d %d\n", &x, &y);
+
+        if (width == -1 && height == -1) {
+            width = x;
+            height = y;
+        } else {
+          // Ensure cell isn't already alive
+          Cell* c = getCellPointer(map, x, y);
+          if (!(*c).nextGenAlive) {
+              changeAliveStatus(map, ALIVE, x, y);
+          }
         }
     } 
     fclose(input);
